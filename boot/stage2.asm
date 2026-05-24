6eda11efbb0fc66d8c09365f250383e5ccf82d73
Joao Cardoso Dias <joao.dias@sou.inteli.edu.br>
2026-05-24 12:25:10 -0300

    refactor: move kernel project to sub-directory and transition bootloader to 64-bit long mode

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -1,5 +1,3 @@
-
-
 [bits 16]
 [org 0x7E00]
 
@@ -12,41 +10,24 @@ _start:
     mov sp, 0x7E00
     sti
 
-lgdt [gdt_ptr]
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+
+    lgdt [gdt_ptr]
     cli
-    mov eax,cr0
-    or eax,1
-    mov cr0,eax
+    mov eax, cr0
+    or  eax, 1
+    mov cr0, eax
     jmp 0x08:protected_mode
 
-[bits 32]
+msg:     db "Boot OK", 0
+msg_len  equ $ - msg
 
-protected_mode:
-    mov ax,0x10
-    mov ds,ax
-    mov es,ax
-    mov fs,ax
-    mov gs,ax
-    mov ss,ax
-
-    mov esp,0x90000
-    mov edi,0xB8000
-    mov esi,msg
-    mov ecx,msg_len
-.write:
-    mov al, [esi]
-    mov [edi],al
-    mov byte [edi+1],0x07
-    add edi,2
-    add esi,1
-    loop .write
-    jmp $
-
-msg:db "BOOT OK-Protected Mode",0
 gdt_start:
     dq 0
 
-gdt_code:
+gdt_code32:
     dw 0xFFFF
     dw 0x0000
     db 0x00
@@ -62,9 +43,76 @@ gdt_data:
     db 11001111b
     db 0x00
 
+gdt_code64:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 10101111b
+    db 0x00
+
 gdt_end:
 
 gdt_ptr:
-    dw gdt_end -gdt_start -1 
+    dw gdt_end - gdt_start - 1
     dd gdt_start
-msg_len equ $ - msg
+
+[bits 32]
+
+protected_mode:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000
+
+    mov edi, 0x10000
+    xor eax, eax
+    mov ecx, 0xC00
+    rep stosd
+
+    mov dword [0x10000], 0x11003
+    mov dword [0x11000], 0x12003
+    mov dword [0x12000], 0x000083
+
+    mov eax, 0x10000
+    mov cr3, eax
+
+    mov eax, cr4
+    or  eax, 0x20
+    mov cr4, eax
+
+    mov ecx, 0xC0000080
+    rdmsr
+    or  eax, 0x100
+    wrmsr
+
+    mov eax, cr0
+    or  eax, 0x80000000
+    mov cr0, eax
+
+    jmp 0x18:long_mode
+
+[bits 64]
+
+long_mode:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+
+    mov rdi, 0xB8000
+    mov rsi, msg
+    mov rcx, msg_len - 1
+.write:
+    mov al, [rsi]
+    mov [rdi], al
+    mov byte [rdi+1], 0x07
+    add rdi, 2
+    add rsi, 1
+    loop .write
+
+    cli
+    jmp $
